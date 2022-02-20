7c6968286ee7f3b7015b9514f4405c161210163a
Guilherme Mendes <boralagui@outlook.com>
2022-02-20 04:13:17 -0300

    Cmmitando pela primeira vez projeto

---
+++ src/main/java/br/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package br.com.api.parkingcontrol.repositories;
+
+import br.com.api.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
